467e9a9fd218ee308be652106bcd554760b103a2
Christopher Tao <me@christophertao.com>
2016-08-05 09:59:09 +1000

    Product List added, pod init

---
+++ OrderBot/OrderBot/Product.swift
@@ -0,0 +1,13 @@
+//
+//  Product.swift
+//  OrderBot
+//
+//  Created by Christopher Tao on 4/08/2016.
+//  Copyright © 2016 Christopher Tao. All rights reserved.
+//
+
+import UIKit
+
+class Product: NSObject {
+
+}
---
+++ OrderBot/OrderBot/ProductTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductTableViewCell.swift
+//  OrderBot
+//
+//  Created by Christopher Tao on 4/08/2016.
+//  Copyright © 2016 Christopher Tao. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
