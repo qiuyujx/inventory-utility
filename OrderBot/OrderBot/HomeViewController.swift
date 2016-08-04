c1cce9e83fc6770c2276010b599e6f97ccfa0f53
Christopher Tao <me@christophertao.com>
2016-08-04 10:23:16 +1000

    added icons

---
+++ OrderBot/OrderBot/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  OrderBot
+//
+//  Created by Christopher Tao on 24/07/2016.
+//  Copyright © 2016 Christopher Tao. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OrderBot/OrderBot/UtilityHub.swift
@@ -0,0 +1,16 @@
+//
+//  UtilityHub.swift
+//  OrderBot
+//
+//  Created by Christopher Tao on 24/07/2016.
+//  Copyright © 2016 Christopher Tao. All rights reserved.
+//
+
+import UIKit
+
+class UtilityHub: NSObject {
+
+    static let darkBlueColor = UIColor(red: 32.0/255.0, green: 83.0/255.0, blue: 165.0/255.0, alpha: 1.0)
+    static let lightBulueColor = UIColor(red: 60.0/255.0, green: 123.0/255.0, blue: 218.0/255.0, alpha: 1.0)
+    
+}
