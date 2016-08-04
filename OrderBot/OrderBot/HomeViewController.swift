cf923dc08c3b9ea47535fec146ee987d84967f0e
Christopher Tao <me@christophertao.com>
2016-08-04 11:19:57 +1000

    Home screen menu done

--- OrderBot/OrderBot/HomeViewController.swift
+++ OrderBot/OrderBot/HomeViewController.swift
@@ -21,7 +21,40 @@ class HomeViewController: UIViewController{
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func productListPressed(sender: UITapGestureRecognizer) {
+        print("product list pressed")
+    }
 
+    @IBAction func productAddPressed(sender: UITapGestureRecognizer) {
+        print("product add presed")
+    }
+    
+    @IBAction func supplierListPressed(sender: UITapGestureRecognizer) {
+        print("supplier list pressed")
+    }
+    
+    @IBAction func supplierAddPressed(sender: UITapGestureRecognizer) {
+        print("supplier add pressed")
+    }
+    
+    @IBAction func orderListPressed(sender: UITapGestureRecognizer) {
+        print("order list pressed")
+    }
+    
+    @IBAction func orderAddPressed(sender: UITapGestureRecognizer) {
+        print("order add pressed")
+    }
+    
+    @IBAction func newOrderPressed(sender: UITapGestureRecognizer) {
+        print("new order pressed")
+    }
+    
+    @IBAction func resumeOrderPressed(sender: UITapGestureRecognizer) {
+        print("resume order pressed")
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
